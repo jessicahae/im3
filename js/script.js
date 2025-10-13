4dcbda5dad52f3e0979d92ec692300f3e2653b6b
Jessica Häseli <jessica.haeseli@stud.fhgr.ch>
2025-10-13 11:21:12 +0200

    etliches css u. html

--- js/script.js
+++ js/script.js
@@ -8,3 +8,23 @@ fetch('https://mmp-im3.jessicahaeseli.ch/php/unload.php')
     .catch(error => {
         console.error('Error:', error);
     });
+
+const swiper = new Swiper('#swiper-container', {
+    direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 0,
+  centeredSlides: true,
+  mousewheel: true,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: '#swiper-button-next',
+    prevEl: '#swiper-button-prev',
+  },
+  speed: 700,
+  rtl: true,
+});
+
+
+
