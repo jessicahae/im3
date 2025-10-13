f71480d487f7ce66d212a65635bb194a194d5ca0
Jessica Häseli <jessica.haeseli@stud.fhgr.ch>
2025-10-13 11:46:32 +0200

    temperature

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 console.log("Hello, World!");
 
 fetch('https://mmp-im3.jessicahaeseli.ch/php/unload.php')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+const startButton = document.querySelector('#startButton');
+
+startButton.addEventListener('click', () => {
+  // Weiterleitung zu slides.html
+  window.location.href = 'slides.html';
+});
+
+
 
 const swiper = new Swiper('#swiper-container', {
-    direction: 'horizontal',
+  direction: 'horizontal',
   slidesPerView: 1,
   spaceBetween: 0,
   centeredSlides: true,
